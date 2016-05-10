da73e659ed4f1b303fe6fee2d2f6b8c642f151f0
Nedja <nedjapejasinovic@gmail.com>
2016-05-11 00:31:12 +0200

    objects

---
+++ Telebot/Objects.cs
@@ -0,0 +1,287 @@
+﻿namespace Telebot.Objects
+{
+    using Newtonsoft.Json;
+
+    [JsonObject("User")]
+    public class User
+    {
+        [JsonProperty("id")]
+        public string id;
+        [JsonProperty("first_name")]
+        public string first_name;
+        [JsonProperty("username")]
+        public string username;
+    }
+    [JsonObject("getMe")]
+    public class getMe
+    {
+        [JsonProperty("ok")]
+        public bool ok;
+
+        [JsonProperty("result")]
+        public User result;
+    }
+    [JsonObject("Chat")]
+    public class Chat
+    {
+        [JsonProperty("id")]
+        public int id;
+        [JsonProperty("type")]
+        public string type;
+        [JsonProperty("title")]
+        public string title;
+        [JsonProperty("username")]
+        public string username;
+        [JsonProperty("first_name")]
+        public string first_name;
+        [JsonProperty("last_name")]
+        public string last_name;
+    }
+    [JsonObject("MessageEntity")]
+    public class MessageEntity{
+        [JsonProperty("type")]
+        public string type;
+        [JsonProperty("offset")]
+        public int offset;
+        [JsonProperty("length")]
+        public int length;
+        [JsonProperty("url")]
+        public string url;
+    }
+    [JsonObject("Message")]
+    public class Message
+    {
+        [JsonProperty("message_id")]
+        public int message_id;
+        [JsonProperty("from")]
+        public User from;
+        [JsonProperty("date")]
+        public int date;
+        [JsonProperty("chat")]
+        public Chat chat;
+        [JsonProperty("forward_from")]
+        public User forward_from;
+        [JsonProperty("forward_from_chat")]
+        public Chat forward_from_chat;
+        [JsonProperty("forward_date")]
+        public int forward_date;
+        [JsonProperty("reply_to_message")]
+        public Message reply_to_message;
+        [JsonProperty("text")]
+        public string text;
+        [JsonProperty("entities")]
+        public MessageEntity[] entities;
+        [JsonProperty("audio")]
+        public Audio audio;
+        [JsonProperty("document")]
+        public Document document;
+        [JsonProperty("photo")]
+        public PhotoSize[] photo;
+        [JsonProperty("sticker")]
+        public Sticker sticker;
+        [JsonProperty("video")]
+        public Video video;
+        [JsonProperty("voice")]
+        public Voice voice;
+        [JsonProperty("caption")]
+        public string caption;
+        [JsonProperty("contact")]
+        public string contact;
+        [JsonProperty("location")]
+        public Location location;
+        [JsonProperty("venue")]
+        public Venue venue;
+        [JsonProperty("new_chat_member")]
+        public User new_chat_member;
+        [JsonProperty("left_chat_member")]
+        public User left_chat_member;
+        [JsonProperty("new_chat_title")]
+        public string new_chat_title;
+        [JsonProperty("new_chat_photo")]
+        public PhotoSize[] new_chat_photo;
+        [JsonProperty("delete_chat_photo")]
+        public bool delete_chat_photo;
+        [JsonProperty("group_chat_created")]
+        public bool group_chat_created;
+        [JsonProperty("supergroup_chat_created")]
+        public bool supergroup_chat_created;
+        [JsonProperty("channel_chat_created")]
+        public bool channel_chat_created;
+        [JsonProperty("pinned_message")]
+        public Message pinned_message;
+    }
+    [JsonObject("Update")]
+    public class Update
+    {
+        [JsonProperty("update_id")]
+        public int update_id;
+        [JsonProperty("message")]
+        public Message message;
+        [JsonProperty("inline_query")]
+        public InlineQuery inline_query;
+        [JsonProperty("chosen_inline_result")]
+        public ChosenInlineResult chosen_inline_result;
+        [JsonProperty("callback_query")]
+        public CallbackQuery callback_query;
+    }
+    [JsonObject("Audio")]
+    public class Audio
+    {
+        [JsonProperty("file_id")]
+        public string file_id;
+        [JsonProperty("duration")]
+        public int duration;
+        [JsonProperty("performer")]
+        public string performer;
+        [JsonProperty("title")]
+        public string title;
+        [JsonProperty("mime_type")]
+        public string mime_type;
+        [JsonProperty("file_size")]
+        public int file_size;
+    }
+    [JsonObject("Contact")]
+    public class Contact
+    {
+        [JsonProperty("phone_number")]
+        public string phone_number;
+        [JsonProperty("first_name")]
+        public string first_name;
+        [JsonProperty("last_name")]
+        public string last_name;
+        [JsonProperty("user_id")]
+        public int user_id;
+    }
+    [JsonObject("InlineQuery")]
+    public class InlineQuery
+    {
+        [JsonProperty("id")]
+        public string id;
+        [JsonProperty("from")]
+        public User from;
+        [JsonProperty("location")]
+        public Location location;
+        [JsonProperty("query")]
+        public string query;
+        [JsonProperty("offset")]
+        public string offset;
+    }
+    [JsonObject("ChosenInlineResult")]
+    public class ChosenInlineResult
+    {
+        [JsonProperty("result_id")]
+        public string result_id;
+        [JsonProperty("from")]
+        public User from;
+        [JsonProperty("location")]
+        public Location location;
+        [JsonProperty("inline_message_id")]
+        public string inline_message_id;
+        [JsonProperty("query")]
+        public string query;
+    }
+    [JsonObject("CallbackQuery")]
+    public class CallbackQuery
+    {
+        [JsonProperty("id")]
+        public string id;
+        [JsonProperty("from")]
+        public User from;
+        [JsonProperty("message")]
+        public Message message;
+        [JsonProperty("inline_message_id")]
+        public string inline_message_id;
+        [JsonProperty("data")]
+        public string data;
+    }
+    [JsonObject("Venue")]
+    public class Venue
+    {
+        [JsonProperty("location")]
+        public Location location;
+        [JsonProperty("title")]
+        public string title;
+        [JsonProperty("address")]
+        public string address;
+        [JsonProperty("foursquare_id")]
+        public string foursquare_id;
+    }
+    [JsonObject("Location")]
+    public class Location
+    {
+        [JsonProperty("longitude")]
+        public float longitude;
+        [JsonProperty("latitude")]
+        public float latitude;
+    }
+    [JsonObject("Voice")]
+    public class Voice
+    {
+        [JsonProperty("file_id")]
+        public string file_id;
+        [JsonProperty("duration")]
+        public int duration;
+        [JsonProperty("mime_type")]
+        public string mime_type;
+        [JsonProperty("file_size")]
+        public int file_size;
+    }
+    public class PhotoSize
+    {
+        [JsonProperty("file_id")]
+        public string file_id;
+        [JsonProperty("width")]
+        public int width;
+        [JsonProperty("height")]
+        public int height;
+        [JsonProperty("file_size")]
+        public int file_size;
+    }
+    public class Sticker
+    {
+        [JsonProperty("file_id")]
+        public string file_id;
+        [JsonProperty("width")]
+        public int width;
+        [JsonProperty("height")]
+        public int height;
+        [JsonProperty("thumb")]
+        public PhotoSize thumb;
+        [JsonProperty("emoji")]
+        public string emoji;
+        [JsonProperty("file_size")]
+        public int file_size;
+    }
+    [JsonObject("Video")]
+    public class Video
+    {
+        [JsonProperty("file_id")]
+        public string file_id;
+        [JsonProperty("width")]
+        public int width;
+        [JsonProperty("height")]
+        public int height;
+        [JsonProperty("duration")]
+        public int duration;
+        [JsonProperty("thumb")]
+        public PhotoSize thumb;
+        [JsonProperty("mime_type")]
+        public string mime_type;
+        [JsonProperty("file_size")]
+        public int file_size;
+    }
+    [JsonObject("Document")]
+    public class Document
+    {
+        [JsonProperty("file_id")]
+        public string file_id;
+        [JsonProperty("thumb")]
+        public PhotoSize thumb;
+        [JsonProperty("file_name")]
+        public string file_name;
+        [JsonProperty("mime_type")]
+        public string mime_type;
+        [JsonProperty("file_size")]
+        public int file_size;
+    }
+}
---
+++ Telebot/Program.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Telebot.Objects;
+
+namespace Telebot
+{
+    public class komp
+    {
+        [JsonProperty("CPU")]
+        public string cpu;
+        [JsonProperty("PSU")]
+        public string psu;
+    }
+
+
+
+
+    public class Bot
+    {
+        static void Main(string[] args)
+        {
+            var bot = new Bot("209137847:AAH7uktgrCt1_TGDFdX6-xM80KF9GgNfADE");
+            Console.Out.WriteLine("");
+            Console.Out.WriteLine(bot.getMe());
+            Console.Out.WriteLine("");
+            Console.Out.WriteLine(bot.sendMessage("hello iz charpa"));
+            bot.loop().Wait();
+        }
+
+        public string Key;
+
+        public Bot(string key)
+        {
+            Key = key;
+        }
+
+        public async Task loop()
+        {
+            while (true)
+            {
+                WebRequest request = WebRequest.Create("https://api.telegram.org/bot" + Key + "/getUpdates");
+                var response = request.GetResponse();
+                System.Diagnostics.Debug.WriteLine(((HttpWebResponse)response).StatusDescription);
+                // Get the stream containing content returned by the server.
+                Stream dataStream = response.GetResponseStream();
+                System.Diagnostics.Debug.WriteLine("");
+                // Open the stream using a StreamReader for easy access.
+                StreamReader reader = new StreamReader(dataStream);
+                // Read the content.
+                string responseFromServer = reader.ReadToEnd();
+
+                JsonTextReader jreader = new JsonTextReader(new StringReader(responseFromServer));
+                var ob = JsonSerializer.Create().Deserialize<Update[]>(jreader)[0];
+
+                // Display the content.
+                System.Diagnostics.Debug.WriteLine(responseFromServer);
+                System.Diagnostics.Debug.WriteLine("");
+                System.Diagnostics.Debug.WriteLine("");
+                // Clean up the streams and the response.
+                reader.Close();
+                response.Close();
+
+                await Task.Delay(1000);
+            }
+        }
+
+        public string getMe()
+        {
+            WebRequest request = WebRequest.Create("https://api.telegram.org/bot" + Key + "/getMe");
+            var response = request.GetResponse();
+            Console.WriteLine(((HttpWebResponse)response).StatusDescription);
+            // Get the stream containing content returned by the server.
+            Stream dataStream = response.GetResponseStream();
+            // Open the stream using a StreamReader for easy access.
+            StreamReader reader = new StreamReader(dataStream);
+            // Read the content.
+            string responseFromServer = reader.ReadToEnd();
+
+            JsonTextReader jreader = new JsonTextReader(new StringReader(responseFromServer));
+            var ob = JsonSerializer.Create().Deserialize<getMe>(jreader);
+
+            // Display the content.
+            Console.WriteLine(responseFromServer);
+            // Clean up the streams and the response.
+            reader.Close();
+            response.Close();
+
+            return responseFromServer;
+        }
+
+        public string sendMessage(string text)
+        {
+            WebRequest request = WebRequest.Create("https://api.telegram.org/bot" + Key + "/sendMessage?chat_id=180771137&text=" + text);
+
+            var response = request.GetResponse();
+            Console.WriteLine(((HttpWebResponse)response).StatusDescription);
+            // Get the stream containing content returned by the server.
+            Stream dataStream = response.GetResponseStream();
+            // Open the stream using a StreamReader for easy access.
+            StreamReader reader = new StreamReader(dataStream);
+            // Read the content.
+            string responseFromServer = reader.ReadToEnd();
+            string json = @"{
+                    'CPU': 'Intel',
+                    'PSU': '500W'
+            }";
+
+            JsonTextReader jreader = new JsonTextReader(new StringReader(json));
+            var ob = JsonSerializer.Create().Deserialize<komp>(jreader);
+
+            while (jreader.Read())
+            {
+                if (jreader.Value != null)
+                {
+                    Console.WriteLine("Token: {0}, Value: {1}", jreader.TokenType, jreader.Value);
+                }
+                else
+                {
+                    Console.WriteLine("Token: {0}", jreader.TokenType);
+                }
+            }
+            // Display the content.
+            Console.WriteLine(responseFromServer);
+            // Clean up the streams and the response.
+            reader.Close();
+            response.Close();
+
+            return responseFromServer;
+        }
+
+    }
+}
